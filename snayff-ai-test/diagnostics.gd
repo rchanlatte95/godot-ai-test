07efed7dcac7a54f723cb95ba60550a7401da4ab
Ryan Chanlatte <113424043+rchanlatte95@users.noreply.github.com>
2024-08-31 22:19:59 -0400

    Fixed stat bug

--- snayff-ai-test/diagnostics.gd
+++ snayff-ai-test/diagnostics.gd
@@ -1,11 +1,98 @@
-extends Node2D
+extends AspectRatioContainer
 
+@export var TimeStats: Label
+@export var MemStats: Label
+@export_range(5, 30) var MinutesToTrack: int
 
-# Called when the node enters the scene tree for the first time.
+const MAX_FPS: int = 999
+const TARGET_FPS: int = 72
+const MILISECS_PER_SEC: int = 1000
+const SECS_PER_MIN: int = 60
+const MINS_PER_HOUR: int = 60
+const SECS_PER_HOUR: int = (SECS_PER_MIN * MINS_PER_HOUR)
+
+const MAX_FRAMES_PER_MIN: int = (MAX_FPS * SECS_PER_MIN)
+const MAX_FRAMES_PER_HOUR: int = (MAX_FPS * SECS_PER_HOUR)
+
+const LOWEST_PCT_RANGE: float = 0.05
+
+var file_buffer: PackedFloat32Array
+
+var avg_buffer_idx: int = 0
+var TARGET_LOW_FRAME_CT: int = 10
+var AVG_BUFFER_SIZE: int = (int)(TARGET_LOW_FRAME_CT / LOWEST_PCT_RANGE)
+var avg_buffer: PackedFloat32Array
+var lowest_fps: float = 1000.0 * 1000.0
+var accum: float = 0.0
+var lowest_avg_fps: float = 0.0
+var trim_avg_fps: float = 0.0
+
+var current_fps_fmt = "%10.3f FPS | LOWEST FPS: %6.3f | AVERAGE FPS: %10.3f | 5%% LOW FPS: %6.3f"
+
+func CalculateLowestFPSWeightedAverage(low_frames: PackedFloat32Array) -> float:
+	var avg: float = 0.0
+	var half_frame_ct = TARGET_LOW_FRAME_CT / 2
+	
+	var slowest_frame_weight_proportion = 0.5
+	avg += low_frames[0] * slowest_frame_weight_proportion
+	
+	var slower_frame_weight_proportion = 0.3
+	var slower_frame_weight: float = slower_frame_weight_proportion / (half_frame_ct - 1)
+	for i in range(1, half_frame_ct):
+		avg += low_frames[i] * slower_frame_weight
+	
+	var slow_frame_weight: float = 1.0 - (slowest_frame_weight_proportion + slower_frame_weight_proportion)
+	slow_frame_weight /= half_frame_ct
+	var test: float = 0.0
+	for i in range(half_frame_ct, TARGET_LOW_FRAME_CT):
+		avg += low_frames[i] * slow_frame_weight
+		test += slow_frame_weight
+		
+	
+	return avg
+
+func CalculateTrimmedFpsAverage(trimmed_frames: PackedFloat32Array) -> float:
+	var avg: float = 0.0
+	for frame_time: float in trimmed_frames:
+		avg += frame_time
+	return avg / trimmed_frames.size()
+
+# Instead of 1% low FPS, we're doing a weighted average on the lowest 5%
+func TabulateTimeStatistics(delta: float) -> void:
+	
+	var fps: float = 1.0 / delta
+	avg_buffer[avg_buffer_idx] = fps
+	avg_buffer_idx = avg_buffer_idx + 1
+	lowest_fps = fps if fps < lowest_fps else lowest_fps
+	
+	if (avg_buffer_idx >= AVG_BUFFER_SIZE):
+		avg_buffer_idx = 0
+		avg_buffer.sort()
+		lowest_avg_fps = CalculateLowestFPSWeightedAverage(avg_buffer.slice(0, TARGET_LOW_FRAME_CT))
+		trim_avg_fps = CalculateTrimmedFpsAverage(avg_buffer.slice(TARGET_LOW_FRAME_CT, (AVG_BUFFER_SIZE - TARGET_LOW_FRAME_CT)))
+		avg_buffer.fill(0.0)
+	else:
+		
+		accum += delta
+		if (accum <= 0.1):
+			return
+		accum = 0.0
+		
+		var final_str = current_fps_fmt % [fps, lowest_fps, trim_avg_fps, lowest_avg_fps]
+		TimeStats.text = final_str
+	
+
 func _ready() -> void:
-	pass # Replace with function body.
+	TimeStats.text = ""
+	MemStats.text = ""
+	
+	avg_buffer.resize(AVG_BUFFER_SIZE)
+	
+	var file_buffer_sz: int = MinutesToTrack * SECS_PER_MIN
+	file_buffer.resize(file_buffer_sz)
+	
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	TabulateTimeStatistics(delta)
 	pass
